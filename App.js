7bdef9e3741fb790440047b3935e4414480cb586
Jônatas Alves <alvesjonatas99@gmail.com>
2020-11-03 21:22:00 -0300

    Buscando itens no armazenamento e salvando o path

--- App.js
+++ App.js
@@ -1,22 +1,52 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
 import Video from 'react-native-video';
+import FilePickerManager from 'react-native-file-picker';
+
 const App = () => {
-  const url = 'https://www.w3schools.com/html/mov_bbb.mp4';
-  const type = 0;
+  const [type, setType] = useState(0);
+  const [url, setUrl] = useState('https://www.w3schools.com/html/mov_bbb.mp4');
+  const [newList, setNewList] = useState([]);
+  const array = useState([]);
+
+  function onEnd() {
+    setType(1);
+  }
+
+  function selectFile() {
+    FilePickerManager.showFilePicker(null, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled file picker');
+      } else if (response.error) {
+        console.log('FilePickerManager Error: ', response.error);
+      } else {
+        setNewList([response.uri].concat(newList));
+        setNewList((newList) => [...newList, response.uri]);
+        console.log('-----------' + newList);
+      }
+    });
+  }
+
   return (
     <>
       {type === 0 ? (
         <Video
-          source={{uri: 'https://www.w3schools.com/html/mov_bbb.mp4'}} // Can be a URL or a local file.
+          source={{
+            uri: url,
+          }} // Can be a URL or a local file.
           style={styles.backgroundVideo}
+          onEnd={() => onEnd()} // Callback when playback finishes
           fullscreen={true}
           fullscreenOrientation={'landscape'}
-          repeat={true}
           resizeMode={'cover'}
         />
       ) : (
-        <Text>Imagem</Text>
+        <>
+          <Text>{newList}</Text>
+          <Button title="Teste" onPress={() => selectFile()} />
+        </>
       )}
     </>
   );
