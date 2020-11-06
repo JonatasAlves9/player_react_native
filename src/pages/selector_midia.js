48dbf61d84c27be788c32574a8cead8e9209c3b2
JoaoTrs <joaorafaelt_2010@hotmail.com>
2020-11-06 14:57:42 -0300

    selector midias

--- src/pages/selector_midia.js
+++ src/pages/selector_midia.js
@@ -5,22 +5,59 @@ const styles = StyleSheet.create({
 
     text_main: {
 
-        color: "#000"
+        color: "#145585",
+        marginTop: 35,
+        marginLeft: 50,
+        fontSize: 19,
 
     },
+
+    text_main2: {
+
+        color: "#145585",
+        marginTop: 9,
+        marginLeft: 10,
+        fontSize: 18,
+
+    },
+
+    top: {
+
+        flex: 0.3,
+        backgroundColor: "#60aee6",
+        
+    },
+
+    button_main: {
+
+        width: 400,
+        borderRadius: 6,
+        marginLeft: 150,
+        marginRight: 50,
+        alignItems: "center",
+        backgroundColor: "#60aee6",
+        marginBottom: 6,
+        marginTop: 160,
+
+    },
+     
 
 })
 
 export default function selector_midia() {
 
     return (
-        <View>
+        <View style={styles.top}>
 
-            <ScrollView>
+            
 
             <Text style={styles.text_main}>Selecione as mídias para o player</Text>
 
-            </ScrollView>
+            <TouchableOpacity style={styles.button_main}>
+            
+                    <Text style={styles.text_main2}>+ Adicionar</Text>
+
+                </TouchableOpacity>
 
         </View>
     )
