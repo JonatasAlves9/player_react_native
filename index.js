b86f14e08213b55c5cb5cd5f25f7c83044aa5ee9
JoaoTrs <joaorafaelt_2010@hotmail.com>
2020-11-09 13:39:02 -0300

    Tela de License

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import selector_midia from './src/pages/selector_midia'
+import screen_license from './src/pages/screen_license'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => selector_midia);
+AppRegistry.registerComponent(appName, () => screen_license);
